172cdbc4e00a07268c37d04b61c9574fc746dfb1
zelim <zelimkhanjachichanov@hotmail.com>
2024-10-02 16:39:32 +0200

    Set-up WikipediaScraper class

---
+++ src/scraper.py
@@ -0,0 +1,21 @@
+class WikipediaScraper:
+    def __init__(self) -> None:
+        pass
+
+    def refresh_cookies() -> object:
+        pass
+
+    def get_countries() -> str:
+        pass
+
+    def get_leaders(country: str) -> str:
+        pass
+
+    def get_first_paragraph(wikipedia_url: str) -> str:
+        pass
+
+    def to_json_file(filepath: str) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
