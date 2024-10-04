5fe07cc117b68f440f978f2a1a466283ea8bf361
zelim <zelimkhanjachichanov@hotmail.com>
2024-10-04 16:12:37 +0200

    README and docstrings

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from src.scraper import WikipediaScraper
 
+# Initializing WikipediaScraper object
 scraper = WikipediaScraper()
 
+# Get list of countries from API
 countries = scraper.get_countries()
+
+# Loop through the list and get the leaders for each country
 for country in countries:
     scraper.get_leaders(country)
 
+# Saving dictionary to a json file
 scraper.to_json_file("leaders_data")
